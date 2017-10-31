eac5c6fb88bb0cc75ff72a5ea2ff27bfbb967e47
akahn-github <ak@tinymanager.com>
2017-10-31 08:58:05 -0500

    Add App Component

---
+++ src/Components/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+import SearchBar from '../SearchBar/SearchBar.js'
+import SearchResults from '../SearchResults/SearchResults.js'
+import PlayList from '../PlayList/PlayList.js'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* Add a SearchBar component */}
+          <div className="App-playlist">
+            {/* Add a SearchResults component */}
+            {/* Add a Playlist component*/}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
