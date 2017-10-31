af59a157840ecf5023df698ebbdaf342eb76691f
akahn-github <ak@tinymanager.com>
2017-10-31 13:00:27 -0500

    Add SearchResults Component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SearchBar.css'
+
+import TrackList from '../TrackList/TrackList.js'
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/*  Add a TrackList component */}
+      </div>
+    )
+  }
+}
+
+export default SearchBar
